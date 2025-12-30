0a8c5203b68e4fcb78596beb0537c98afcb5ad40
Davide <3d.davidecapuzzo@gmail.com>
2025-12-30 17:39:52 -0600

    save opencv

--- src/logger.cpp
+++ src/logger.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 #include "state_saver/logger.hpp"
-#include <cv_bridge/cv_bridge.h>               // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
+#include <cv_bridge/cv_bridge.hpp>               // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
 #include <image_transport/image_transport.hpp> // Using image_transport allows us to publish and subscribe to compressed image streams in ROS2
 #include <opencv2/opencv.hpp>                  // We include everything about OpenCV as we don't care much about compilation time at the moment.
 #include "common/common.hpp"
